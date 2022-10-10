6f7b8d06dbee75df9c2b3ab88981aea841b7b4f0
Fredrik J <fredrikj.hem@gmail.com>
2022-10-10 20:40:04 +0200

    Started to work with cyclotomic matrices

---
+++ src/generate_cyclotomic.py
@@ -0,0 +1,6 @@
+"""Generates all cyclotomic polynomials that generates m x m 
+cyclotomic polynomials over F_q."""
+
+if __name__ == '__main__':
+    print("Generating all cyclotomic polynomials")
+
---
+++ src/pct/__init__.py
@@ -0,0 +1,1 @@
+"""pct or python coding theory is a library for working with linear codes in Python"""
